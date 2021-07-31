c3bfa995097b9e11257bc6ecc0a478e83d16074f
Yusuf Ayuba <yusufayuba@live.com>
2021-07-31 14:21:11 +0800

    add migration links

---
+++ yii/migrations/m210731_061740_links.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210731_061740_links
+ */
+class m210731_061740_links extends Migration
+{
+    public $table_name = 'links';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable($this->table_name, [
+            'id' => $this->primaryKey(),
+            'alias' => $this->string(50)->notNull()->unique(),
+            'url_string' => $this->string(255)->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210731_061740_links cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210731_061740_links cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
